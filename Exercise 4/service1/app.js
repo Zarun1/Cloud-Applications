b84058606d035407d0981c8a55c12906332695be
pulkkin3 <taneli.pulkkinen@student.tut.fi>
2019-10-01 21:08:25 +0300

    Removed commented out code from Service1.

--- Exercise 4/service1/app.js
+++ Exercise 4/service1/app.js
@@ -1,26 +1,6 @@
 const http = require('http');
 const axios = require('axios').default;
 
-// http.createServer(function (request, response) {
-//
-//   axios.get('http://localhost:8893/')
-//     .then(response2 => {
-//       console.log('response2 ', response2.data);
-//       response.write(response2.data.toString());
-//       response.writeHead(200, {'Content-type': 'text/plain'});
-//     })
-//     .catch(error => {
-//       console.log('error ', error);
-//       response.write('Service2 responded with error ', error);
-//       response.writeHead(500, {'Content-type': 'text/plain'});
-//     })
-//     .finally(() => {
-//       console.log('end ');
-//       response.end();
-//       return response;
-//     });
-// }).listen(8001);
-
 http.createServer(function (request, response) {
   let options = {
       host: 'localhost',
